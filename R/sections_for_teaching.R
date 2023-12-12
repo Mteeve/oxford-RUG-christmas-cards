80eb31f4d61a42d96043f000822b2d52f96a483d
Kasia Banas <kasia.banas@gmail.com>
2023-12-12 18:47:51 +0000

    add file with sections for teaching

---
+++ R/sections_for_teaching.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(ggforce)
+
+# background (midnight sky)
+
+# snow on ground
+
+# add snowflakes
+
+# add snowman
+
+# add eyes and buttons
+
+# add sticks for arms
+
+# add hat
+
+# add nose
+
+# add text
