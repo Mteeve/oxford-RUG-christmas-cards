bb42de5a7b63c09ae8a3b25dc4670f8b91363f76
John Wilson <jwilso3@ed.ac.uk>
2023-12-13 10:36:56 +0000

    Add random snowflakes

--- R/sections_for_teaching.R
+++ R/sections_for_teaching.R
@@ -9,9 +9,33 @@ s1
 # snow on ground
 s2 <- s1 +
   annotate(
-    geom = "rect"
-  )
+    geom = "rect",
+    xmin = 0,
+    xmax = 1,
+    ymin = 0,
+    ymax = 0.2,
+    fill = 'grey98',
+    colour = 'grey98'
+  ) +
+  xlim(0, 1) +
+  ylim(0, 1) +
+  coord_fixed(expand = FALSE)
+
+s2
 # add snowflakes
+
+set.seed(20231225)
+n <- 100
+
+snowflakes <- data.frame(x = runif(n),
+                         y = runif(n))
+
+s3 <- s2 +
+  geom_point(data = snowflakes,
+             mapping = aes(x = x, y = y),
+             colour = 'white',
+             shape = 8)
+s3
 
 # add snowman
 
