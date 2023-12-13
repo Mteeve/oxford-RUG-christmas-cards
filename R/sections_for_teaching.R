db2f568bee25fd5f5426a7cd774376b729180573
Kasia Banas <kasia.banas@gmail.com>
2023-12-13 10:20:02 +0000

    added blue background

--- R/sections_for_teaching.R
+++ R/sections_for_teaching.R
@@ -2,9 +2,15 @@ library(tidyverse)
 library(ggforce)
 
 # background (midnight sky)
-
+s1 <- ggplot() +
+  theme_void() +
+  theme(plot.background = element_rect(fill = "midnightblue"))
+s1
 # snow on ground
-
+s2 <- s1 +
+  annotate(
+    geom = "rect"
+  )
 # add snowflakes
 
 # add snowman
